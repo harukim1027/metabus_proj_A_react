05dabb5e65213aec170edf80f69a352c14f0e6e5
Suin Kim <soo.officer@gmail.com>
2022-02-13 00:10:28 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ import PageLoginForm from 'Pages/PageAccounts/PageLoginForm';
 import PageProfile from 'Pages/PageAccounts/PageProfile';
 import PageSignupForm from 'Pages/PageAccounts/PageSignupForm';
 import PageCheckSignup from 'Pages/PageAccounts/PageCheckSignup';
-<<<<<<< HEAD
-import PageAnimalList from 'Pages/PageStreetanimal/PageAnimalList';
-import PageAnimalDetail from 'Pages/PageStreetanimal/PageAnimalDetail';
-import PageAnimalForm from 'Pages/PageStreetanimal/PageAnimalForm';
-import PageReviewIndex from 'Pages/PageReview/PageReviewIndex';
-import PageReviewDetail from 'Pages/PageReview/PageReviewDetail';
-import PageReviewForm from 'Pages/PageReview/PageReviewForm';
-import PageNoticeList from 'Pages/PageNotice/PageNoticeList';
-import PageNoticeDetail from 'Pages/PageNotice/PageNoticeDetail';
-import PageNoticeForm from 'Pages/PageNotice/PageNoticeForm';
-import PageAssignmentList from 'Pages/PageAssignment/PageAssignmentList';
-=======
 // admin
 import PageAdminMain from 'Pages/PageAdmin/PageAdminMain';
 // admin/Animal
@@ -27,7 +15,6 @@ import PageAnimalList from 'Pages/PageStreetanimal/PageAnimalList';
 import PageAnimalDetail from 'Pages/PageStreetanimal/PageAnimalDetail';
 import PageAnimalForm from 'Pages/PageStreetanimal/PageAnimalForm';
 // admin/UserManagement
->>>>>>> c8d42c15bedd57b74a4c3d8966a6581012635833
 import PageUserManagementIndex from 'Pages/PageUserManagement/PageUserManagementIndex';
 import PageUserManagementDetail from 'Pages/PageUserManagement/PageUserManagementDetail';
 import PageUserManagementForm from 'Pages/PageUserManagement/PageUserManagementForm';
@@ -109,19 +96,6 @@ function App() {
             />
             <Route path="/inquiry/new/" element={<PageInquiryForm />} />
 
-<<<<<<< HEAD
-            {/* ------------admin------------ */}
-            <Route path="/admin/main/" element={<PageAdminMain />} />
-
-            {/* UserManagement */}
-            <Route
-              path="/usermanagement/"
-              element={<PageUserManagementIndex />}
-            />
-            <Route
-              path="/usermanagement/:usermanagementId/"
-              element={<PageUserManagementDetail />}
-=======
             {/* notice */}
             <Route path="/notice/" element={<PageNoticeList />} />
             <Route path="/notice/new/" element={<PageNoticeForm />} />
@@ -129,7 +103,6 @@ function App() {
             <Route
               path="/notice/:noticeId/edit/"
               element={<PageNoticeForm />}
->>>>>>> c8d42c15bedd57b74a4c3d8966a6581012635833
             />
 
             {/* review */}
@@ -137,13 +110,8 @@ function App() {
             <Route path="/review/:reviewId/" element={<PageReviewDetail />} />
             <Route path="/review/new/" element={<PageReviewForm />} />
             <Route
-<<<<<<< HEAD
-              path="/usermanagement/:usermanagementId/edit/"
-              element={<PageUserManagementForm />}
-=======
               path="/review/:reviewId/edit/"
               element={<PageReviewForm />}
->>>>>>> c8d42c15bedd57b74a4c3d8966a6581012635833
             />
           </Routes>
         </div>
