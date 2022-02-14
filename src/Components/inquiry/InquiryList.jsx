44f6053b809ae4a055ae226d1bcd52cd4a578c3b
namuns <skymsn214@gmail.com>
2022-02-14 13:35:47 +0900

    inquiry 검색 구현

--- src/Components/inquiry/InquiryList.jsx
+++ src/Components/inquiry/InquiryList.jsx
@@ -3,39 +3,65 @@ import { useApiAxios } from 'api/base';
 import InquirySummary from './InquirySummary';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from 'contexts/AuthContext';
+import { useState } from 'react';
 
 function InquiryList() {
   const { auth } = useAuth();
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   const [{ data: inquiryList }, refetch] = useApiAxios(
     {
-      url: `/inquiry_board/api/inquiry/`,
+      url: `/inquiry_board/api/inquiry/${query ? '?query=' + query : ''}`,
       method: 'GET',
     },
     {
       manual: true,
     },
   );
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      refetch();
+      console.log('ENTER');
+    }
+  };
+
   useEffect(() => {
     refetch();
   }, []);
 
   return (
-    <div className="my-5 ">
-      {inquiryList && (
-        <div className="flex space-x-1">
-          {inquiryList.map((inquiry) => (
-            <div key={inquiry.inquiry_no}>
-              {auth.userID === inquiry.user && (
-                <InquirySummary inquiry={inquiry} />
-              )}
-              {auth.is_staff && <InquirySummary inquiry={inquiry} />}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <input
+        type="text"
+        placeholder="검색어를 입력해주세요."
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button type="submit" onClick={() => refetch()}>
+        검색
+      </button>
+      <div className="my-5 ">
+        {inquiryList && (
+          <div className="flex space-x-1">
+            {inquiryList.map((inquiry) => (
+              <div key={inquiry.inquiry_no}>
+                {auth.userID === inquiry.user && (
+                  <InquirySummary inquiry={inquiry} />
+                )}
+                {auth.is_staff && <InquirySummary inquiry={inquiry} />}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 export default InquiryList;
