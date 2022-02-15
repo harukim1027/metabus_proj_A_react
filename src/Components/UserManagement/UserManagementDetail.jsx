2906e37a35203244f913ca7ee37796dc526dddc9
Suin Kim <soo.officer@gmail.com>
2022-02-15 11:05:26 +0900

    UserManagement 수정

--- src/Components/UserManagement/UserManagementDetail.jsx
+++ src/Components/UserManagement/UserManagementDetail.jsx
@@ -3,14 +3,13 @@ import { useAuth } from 'contexts/AuthContext';
 import { useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
-function UserManagementDetail({}) {
+function UserManagementDetail({ userId }) {
   const { auth } = useAuth();
   const navigate = useNavigate();
-  const { managementId } = useParams();
 
-  const [{ data: management, loading, error }, refetch] = useApiAxios(
+  const [{ data: userData, loading, error }, refetch] = useApiAxios(
     {
-      url: `/accounts/api/users/${managementId}/`,
+      url: `/accounts/api/users/${userId}/`,
       method: 'GET',
       headers: {
         Authorization: `Bearer ${auth.access}`,
@@ -22,7 +21,7 @@ function UserManagementDetail({}) {
   const [{ loading: deleteLoading, error: deleteError }, deleteUser] =
     useApiAxios(
       {
-        url: `/accounts/api/users/${managementId}/`,
+        url: `/accounts/api/users/${userId}/`,
         method: 'DELETE',
         headers: {
           Authorization: `Bearer ${auth.access}`,
@@ -41,62 +40,62 @@ function UserManagementDetail({}) {
 
   useEffect(() => {
     refetch();
-  }, []);
+  }, [userId]);
 
   return (
     <div>
       <h2>UserManagementDetail</h2>
 
-      {management && (
+      {userData && (
         <>
           <div className="my-3">
             <span>유저아이디</span>
             <span className="border-2 border-sky-400 rounded p-1 ml-2">
-              {management.userID}
+              {userData.userID}
             </span>
           </div>
 
           <div className="my-3">
             <span>이름</span>
             <span className="border-2 border-sky-400 rounded p-1 ml-2">
-              {management.name}
+              {userData.name}
             </span>
           </div>
 
           <div className="my-3">
             <span>닉네임</span>
             <span className="border-2 border-sky-400 rounded p-1 ml-2">
-              {management.nickname}
+              {userData.nickname}
             </span>
           </div>
 
           <div className="my-3">
             <span>연락처</span>
             <span className="border-2 border-sky-400 rounded p-1 ml-2">
-              {management.phone_number}
+              {userData.phone_number}
             </span>
           </div>
 
           <div className="my-3">
             <span>이메일</span>
             <span className="border-2 border-sky-400 rounded p-1 ml-2">
-              {management.email}
+              {userData.email}
             </span>
           </div>
 
           <div className="my-3">
             <span>거주지역</span>
             <span className="border-2 border-sky-400 rounded p-1 ml-2">
-              {management.region === 1 && 'Seoul'}
-              {management.region === 2 && 'Busan'}
-              {management.region === 3 && 'Daegu'}
-              {management.region === 4 && 'Incheon'}
-              {management.region === 5 && 'Daejeon'}
-              {management.region === 6 && 'Sejong'}
-              {management.region === 7 && 'Gwangju'}
-              {management.region === 8 && 'Ulsan'}
-              {management.region === 9 && 'Jeju'}
-              {management.region === 10 && 'Gangwon'}
+              {userData?.region === '1' && 'Seoul'}
+              {userData?.region === '2' && 'Busan'}
+              {userData?.region === '3' && 'Daegu'}
+              {userData?.region === '4' && 'Incheon'}
+              {userData?.region === '5' && 'Daejeon'}
+              {userData?.region === '6' && 'Sejong'}
+              {userData?.region === '7' && 'Gwangju'}
+              {userData?.region === '8' && 'Ulsan'}
+              {userData?.region === '9' && 'Jeju'}
+              {userData?.region === '10' && 'Gangwon'}
             </span>
           </div>
         </>
@@ -106,10 +105,7 @@ function UserManagementDetail({}) {
         <button onClick={handleDelete} className="hover:text-red-400">
           삭제
         </button>
-        <Link
-          to={`/management/${managementId}/edit/`}
-          className="hover:text-red-400"
-        >
+        <Link to={`/management/${userId}/edit/`} className="hover:text-red-400">
           수정
         </Link>
         <Link to="/management/" className="hover:text-red-400">
--- src/Pages/PageUserManagement/PageUserManagementDetail.jsx
+++ src/Pages/PageUserManagement/PageUserManagementDetail.jsx
@@ -3,12 +3,12 @@ import UserManagementDetail from 'Components/UserManagement/UserManagementDetail
 import { useParams } from 'react-router-dom';
 
 function PageUserManagementDetail() {
-  const { usermanagementId } = useParams();
+  const { userId } = useParams();
 
   return (
     <div>
       <TopNav />
-      <UserManagementDetail usermanagementId={usermanagementId} />
+      <UserManagementDetail userId={userId} />
     </div>
   );
 }
