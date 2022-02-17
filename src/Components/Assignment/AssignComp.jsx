993ba83b631136f26833e65aeb70daf5719c8c0b
Yeol Choi <chlduf153@gmail.com>
2022-02-17 16:29:59 +0900

    입양신청 완료시 마이페이지 입양신청 디테일 볼 수 있도록 설정

--- src/Components/Assignment/AssignComp.jsx
+++ src/Components/Assignment/AssignComp.jsx
@@ -49,14 +49,9 @@ function AssignComp({ assignId, assignData }) {
       <div className="flex justify-center mt-5">
         <button
           className="shadow bg-green-400 w-1/5 py-2 rounded-lg hover:bg-green-600 hover:text-white"
-          onClick={() => navigate('/mypage/assigninfo/')}
+          onClick={() => navigate(`/assignment/${assignData?.assignment_no}/`)}
         >
-          마이페이지
-        </button>
-      </div>
-      <div className="flex justify-center mt-5">
-        <button className="shadow bg-blue-400 w-1/5 py-2 rounded-lg hover:bg-blue-600 hover:text-white">
-          신청 정보
+          마이페이지 - 신청 정보
         </button>
       </div>
       <div className="flex justify-center mt-5 mb-20">
