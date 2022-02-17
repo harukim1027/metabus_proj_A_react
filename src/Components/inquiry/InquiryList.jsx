68b0eb5136af2543e248524ee05ee74b553b274b
namuns <skymsn214@gmail.com>
2022-02-17 16:16:23 +0900

    1:1 문의 css 개선, summary 제거

--- src/Components/inquiry/InquiryList.jsx
+++ src/Components/inquiry/InquiryList.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { useApiAxios } from 'api/base';
-import InquirySummary from './InquirySummary';
 import { useAuth } from 'contexts/AuthContext';
 import { useState } from 'react';
 import Sidebar from 'Components/Mypage/Sidebar';
+import { useNavigate } from 'react-router-dom';
 
 function InquiryList() {
   const { auth } = useAuth();
+  const navigate = useNavigate();
   const [query, setQuery] = useState('');
 
   const [{ data: inquiryList }, refetch] = useApiAxios(
@@ -48,20 +49,55 @@ function InquiryList() {
         <button type="submit" onClick={() => refetch()}>
           검색
         </button>
-        <div className="inline-block">
+
+        <table className="m-2">
+          <thead>
+            <tr>
+              <th className="border-2 border-gray-400 p-2">문의 번호</th>
+              <th className="border-2 border-gray-400 p-2">아이디</th>
+              <th className="border-2 border-gray-400 p-2">제목</th>
+              <th className="border-2 border-gray-400 p-2">문의 일자</th>
+              <th className="border-2 border-gray-400 p-2">답변 상태</th>
+            </tr>
+          </thead>
           {inquiryList && (
-            <div className="flex space-x-1">
-              {inquiryList.map((inquiry) => (
-                <div key={inquiry.inquiry_no}>
+            <tbody>
+              {inquiryList?.map((inquiry) => (
+                <>
                   {auth.userID === inquiry.user && (
-                    <InquirySummary inquiry={inquiry} />
+                    <tr>
+                      <td
+                        className="border-2 border-gray-400 text-center cursor-pointer p-2"
+                        onClick={() =>
+                          navigate(`/inquiry/${inquiry.inquiry_no}/`)
+                        }
+                      >
+                        {inquiry.inquiry_no}
+                      </td>
+                      <td className="border-2 border-gray-400 text-center p-2">
+                        {inquiry.user}
+                      </td>
+                      <td
+                        className="border-2 border-gray-400 text-center cursor-pointer p-2"
+                        onClick={() =>
+                          navigate(`/inquiry/${inquiry.inquiry_no}/`)
+                        }
+                      >
+                        {inquiry.title}
+                      </td>
+                      <td className="border-2 border-gray-400 text-center p-2">
+                        {inquiry.created_at}
+                      </td>
+                      <td className="border-2 border-gray-400 text-center p-2">
+                        {/* <h2>{inquiry.status}</h2> */}
+                      </td>
+                    </tr>
                   )}
-                  {auth.is_staff && <InquirySummary inquiry={inquiry} />}
-                </div>
+                </>
               ))}
-            </div>
+            </tbody>
           )}
-        </div>
+        </table>
       </div>
     </>
   );
