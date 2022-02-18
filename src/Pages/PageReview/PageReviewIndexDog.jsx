fd82a39b155bb69629d021f2de4c63c81bf84402
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-02-18 17:45:51 +0900

    1) 입양 후기 리스트(전체, 고양이, 강아지) 리스트 페이지 css 개선 2) 공지 페이지 css 개선 3) 입양 신청서 작성 css 개선

--- src/Pages/PageReview/PageReviewIndexDog.jsx
+++ src/Pages/PageReview/PageReviewIndexDog.jsx
@@ -1,29 +1,11 @@
-import { useNavigate } from 'react-router-dom';
 import ReviewListDog from 'Components/review/ReviewListDog';
 import TopNav from 'Components/Main/TopNavi';
-import { useAuth } from 'contexts/AuthContext';
-import { useApiAxios } from 'api/base';
 
 function PageReviewIndexDog() {
-  const { auth } = useAuth();
-  const navigate = useNavigate();
-
   return (
     <div>
       <TopNav />
-
       <ReviewListDog />
-      {auth.isLoggedIn && !auth.is_staff && (
-        <div className="flex place-content-between">
-          <div></div>
-          <button
-            onClick={() => navigate('/review/new/')}
-            className="mx-20 text-white py-2 px-4 uppercase rounded-md bg-red-400 hover:bg-red-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
-          >
-            글쓰기
-          </button>
-        </div>
-      )}
     </div>
   );
 }
