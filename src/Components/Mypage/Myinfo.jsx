6a426f4e6cc06e194e825696c6f9799440fce602
Suin Kim <soo.officer@gmail.com>
2022-02-21 17:48:19 +0900

    마이페이지의 내 회원정보 CSS 수정중

--- src/Components/Mypage/Myinfo.jsx
+++ src/Components/Mypage/Myinfo.jsx
@@ -18,23 +18,34 @@ function Myinfo() {
   }, []);
 
   return (
-    <div>
-      <div className="w-full h-screen bg-blue-100">
-        <div className="flex justify-center">
-          <div className="inline-block mt-10">
-            <h2>내 정보</h2>
-            <h2>이름 : {userData?.name}</h2>
-            <h2>아이디 : {userData?.userID}</h2>
-            <h2>닉네임 : {userData?.nickname}</h2>
-            <h2>이메일 : {userData?.email}</h2>
-            <h2>연락처 : {userData?.phone_number}</h2>
-            <h2>거주지 : {userData?.region}</h2>
-            <h2>비밀번호 퀴즈 : {userData?.password_quiz}</h2>
-            <h2>비밀번호 퀴즈 정답 : {userData?.password_quiz_answer}</h2>
+    <>
+      <div className="header">
+        <div className="justify-center mx-20">
+          <div className="align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="mypage_header rounded-xl shadow-md">
+              <blockquote class="mt-5 text-4xl mb-3 font-semibold italic text-center text-slate-900">
+                <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-green-400 relative inline-block">
+                  <span class="relative text-white">" 내 정보 "</span>
+                </span>
+              </blockquote>
+
+              <div className="mb-5 overflow-hidden border-b border-gray-200">
+                <table>
+                  <h2>이름 : {userData?.name}</h2>
+                  <h2>아이디 : {userData?.userID}</h2>
+                  <h2>닉네임 : {userData?.nickname}</h2>
+                  <h2>이메일 : {userData?.email}</h2>
+                  <h2>연락처 : {userData?.phone_number}</h2>
+                  <h2>거주지 : {userData?.region}</h2>
+                  <h2>비밀번호 퀴즈 : {userData?.password_quiz}</h2>
+                  <h2>비밀번호 퀴즈 정답 : {userData?.password_quiz_answer}</h2>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
