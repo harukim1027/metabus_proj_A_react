6b43acd99f4bd39877d4616db0527b40d689e640
Suin Kim <soo.officer@gmail.com>
2022-02-22 13:45:14 +0900

    마이페이지의 내 회원정보 CSS 수정

--- src/Components/Mypage/Myinfo.jsx
+++ src/Components/Mypage/Myinfo.jsx
@@ -24,85 +24,92 @@ function Myinfo() {
           <div className="align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="mypage_header rounded-xl shadow-md">
               <blockquote class="mt-5 text-4xl mb-3 font-semibold italic text-center text-slate-900">
-                <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-green-400 relative inline-block">
+                <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-blue-900 relative inline-block">
                   <span class="relative text-white">" 내 회원정보 "</span>
                 </span>
               </blockquote>
 
               <div className="mb-5 overflow-hidden border-b border-gray-200">
                 <table className="mb-5 mr-5 border text-center min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        이름
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        아이디
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        닉네임
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        이메일
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        연락처
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        거주지
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        비밀번호 퀴즈
-                      </th>
-                      <th
-                        scope="row"
-                        className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                      >
-                        비밀번호 퀴즈 정답
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td scope="col">{userData?.name}</td>
-                      <td scope="col">{userData?.userID}</td>
-                      <td scope="col">{userData?.nickname}</td>
-                      <td scope="col">{userData?.email}</td>
-                      <td scope="col">{userData?.phone_number}</td>
-                      <td scope="col">{userData?.region}</td>
-                      <td scope="col">{userData?.password_quiz}</td>
-                      <td scope="col">{userData?.password_quiz_answer}</td>
-                    </tr>
-                  </tbody>
-                  {/* <h2>이름 : {userData?.name}</h2>
-                  <h2>아이디 : {userData?.userID}</h2>
-                  <h2>닉네임 : {userData?.nickname}</h2>
-                  <h2>이메일 : {userData?.email}</h2>
-                  <h2>연락처 : {userData?.phone_number}</h2>
-                  <h2>거주지 : {userData?.region}</h2>
-                  <h2>비밀번호 퀴즈 : {userData?.password_quiz}</h2>
-                  <h2>비밀번호 퀴즈 정답 : {userData?.password_quiz_answer}</h2> */}
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      이름
+                    </th>
+                    <td>{userData?.name}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      아이디
+                    </th>
+                    <td>{userData?.userID}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      닉네임
+                    </th>
+                    <td>{userData?.nickname}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      이메일
+                    </th>
+                    <td>{userData?.email}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      연락처
+                    </th>
+                    <td>{userData?.phone_number}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      거주지
+                    </th>
+                    <td>{userData?.region}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      비밀번호 퀴즈
+                    </th>
+                    <td>{userData?.password_quiz}</td>
+                  </tr>
+
+                  <tr>
+                    <th
+                      scope="row"
+                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      비밀번호 퀴즈 정답
+                    </th>
+                    <td>{userData?.password_quiz_answer}</td>
+                  </tr>
                 </table>
               </div>
             </div>
