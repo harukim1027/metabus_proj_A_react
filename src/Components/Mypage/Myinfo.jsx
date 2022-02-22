60e1f986fe43a06be4a0ae3b81df4d5e912ec7ec
Suin Kim <soo.officer@gmail.com>
2022-02-22 15:38:16 +0900

    마이페이지의 내 회원정보 셀 간격 수정

--- src/Components/Mypage/Myinfo.jsx
+++ src/Components/Mypage/Myinfo.jsx
@@ -30,82 +30,58 @@ function Myinfo() {
               </blockquote>
 
               <div className="mb-5 overflow-hidden border-b border-gray-200">
-                <table className="mb-5 mr-5 border text-center min-w-full divide-y divide-gray-200">
+                <table className="mb-5 mr-5 mt-3 border text-center min-w-full divide-y divide-gray-200">
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       이름
                     </th>
                     <td>{userData?.name}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       아이디
                     </th>
                     <td>{userData?.userID}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       닉네임
                     </th>
                     <td>{userData?.nickname}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       이메일
                     </th>
                     <td>{userData?.email}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       연락처
                     </th>
                     <td>{userData?.phone_number}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       거주지
                     </th>
                     <td>{userData?.region}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       비밀번호 퀴즈
                     </th>
                     <td>{userData?.password_quiz}</td>
                   </tr>
 
                   <tr>
-                    <th
-                      scope="row"
-                      className="bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider"
-                    >
+                    <th className="border border-slate-200 bg-gray-50 px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-72">
                       비밀번호 퀴즈 정답
                     </th>
                     <td>{userData?.password_quiz_answer}</td>
