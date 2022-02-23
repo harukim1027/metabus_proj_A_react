b64dfab1c2fd56457e2f077285f3f003fbacbe84
namuns <skymsn214@gmail.com>
2022-02-23 17:36:02 +0900

    review css 수정

--- src/Components/review/ReviewDetail.jsx
+++ src/Components/review/ReviewDetail.jsx
@@ -44,86 +44,102 @@ function ReviewDetail({ reviewId }) {
     <>
       <div className="header">
         <div className="flex flex-wrap justify-center max-w-m">
-          <div className="header justify-center rounded px-20 pt-6 mb-3">
+          <div className="flex header justify-center rounded px-20 pt-6 mb-3">
             <div className="review_header shadow-md overflow-hidden sm:rounded-lg">
               <blockquote class="mt-5 text-3xl font-semibold italic text-center text-slate-900">
-                <span class="mt-3 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-green-400 relative inline-block">
-                  <span class="relative text-white">" 입양후기 "</span>
+                <span class="mt-3 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-purple-400 relative inline-block">
+                  <span class="relative text-white text-6xl">" 입양후기 "</span>
                 </span>
               </blockquote>
-              <div className="px-4 py-5 sm:px-6">
-                {review && (
-                  <>
-                    <h1 className="text-lg leading-6 font-bold text-gray-900">
-                      {review.title}
-                    </h1>
-                    <hr className="mt-3 mb-3" />
+              <div className="flex justify-center">
+                <div className="px-4 py-5 w-2/3">
+                  {review && (
+                    <>
+                      <h1 className="text-lg leading-6 font-bold text-gray-900">
+                        {review.title}
+                      </h1>
+                      <hr className="mt-3 mb-3" />
 
-                    <div className="mb-4">
-                      {review.image1 && (
-                        <img src={review.image1} alt={review.image1} />
-                      )}
-                      {review.image2 && (
-                        <img src={review.image2} alt={review.image2} />
-                      )}
-                      {review.image3 && (
-                        <img src={review.image3} alt={review.image3} />
-                      )}
-                      {review.image4 && (
-                        <img src={review.image4} alt={review.image4} />
-                      )}
-                      {review.image5 && (
-                        <img src={review.image5} alt={review.image5} />
-                      )}
-                    </div>
+                      <div className="mb-4 flex justify-center">
+                        {review.image1 && (
+                          <img
+                            src={review.image1}
+                            alt={review.image1}
+                            className="w-full"
+                          />
+                        )}
+                      </div>
 
-                    {/*  */}
+                      <div>
+                        {review.image2 && (
+                          <img src={review.image2} alt={review.image2} />
+                        )}
+                      </div>
+                      <div>
+                        {review.image3 && (
+                          <img src={review.image3} alt={review.image3} />
+                        )}
+                      </div>
 
-                    <h2 className="sm:mt-0 sm:col-span-2 mt-2 mb-3 max-w-2xl text-sm text-gray-900">
-                      {review.content}
-                    </h2>
-                    <br />
-                    <hr className="mt-3 mb-3" />
+                      <div>
+                        {review.image4 && (
+                          <img src={review.image4} alt={review.image4} />
+                        )}
+                      </div>
+                      <div>
+                        {review.image5 && (
+                          <img src={review.image5} alt={review.image5} />
+                        )}
+                      </div>
 
-                    <h4 className="flex items-center text-gray-400">
-                      <img
-                        src={review.adoptassignment.animal.image}
-                        width="100"
-                        alt=""
-                      />
-                      {review.adoptassignment.animal.end_date}일에{' '}
-                      {review.user.nickname}님께 입양된{' '}
-                      {review.adoptassignment.animal.animal_reg_num} 의 입양
-                      후기입니다.
-                    </h4>
+                      {/*  */}
 
-                    <div className="my-5 text-right">
-                      <Link
-                        className="ml-3 flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                        to="/review/"
-                      >
-                        목록으로
-                      </Link>
-                      {auth.userID === review.user.userID && (
+                      <h2 className="sm:mt-0 sm:col-span-2 mt-2 mb-3 max-w-2xl text-sm text-gray-900">
+                        {review.content}
+                      </h2>
+                      <br />
+                      <hr className="mt-3 mb-3" />
+
+                      <h4 className="flex items-center text-gray-400">
+                        <img
+                          src={review.adoptassignment.animal.image}
+                          width="100"
+                          alt=""
+                        />
+                        {review.adoptassignment.animal.end_date}일에{' '}
+                        {review.user.nickname}님께 입양된{' '}
+                        {review.adoptassignment.animal.animal_reg_num} 의 입양
+                        후기입니다.
+                      </h4>
+
+                      <div className="my-5 text-right">
                         <Link
                           className="ml-3 flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                          to={`/review/${reviewId}/edit/`}
+                          to="/review/"
                         >
-                          수정하기
+                          목록으로
                         </Link>
-                      )}
-                      {(auth.userID === review.user.userID ||
-                        auth.is_staff) && (
-                        <button
-                          className="ml-3 flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                          onClick={() => handleDelete()}
-                        >
-                          삭제하기
-                        </button>
-                      )}
-                    </div>
-                  </>
-                )}
+                        {auth.userID === review.user.userID && (
+                          <Link
+                            className="ml-3 flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                            to={`/review/${reviewId}/edit/`}
+                          >
+                            수정하기
+                          </Link>
+                        )}
+                        {(auth.userID === review.user.userID ||
+                          auth.is_staff) && (
+                          <button
+                            className="ml-3 flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                            onClick={() => handleDelete()}
+                          >
+                            삭제하기
+                          </button>
+                        )}
+                      </div>
+                    </>
+                  )}
+                </div>
               </div>
             </div>
           </div>
