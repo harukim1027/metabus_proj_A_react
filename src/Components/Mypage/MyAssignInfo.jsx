1cc39e08f9252b4c12abbea1a3e4b0a53b9c0621
Yeol Choi <chlduf153@gmail.com>
2022-02-23 16:04:25 +0900

    mypage 스크롤 기능 구현

--- src/Components/Mypage/MyAssignInfo.jsx
+++ src/Components/Mypage/MyAssignInfo.jsx
@@ -1,6 +1,6 @@
 import { useApiAxios } from 'api/base';
 import { useAuth } from 'contexts/AuthContext';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './Mypage.css';
@@ -20,6 +20,38 @@ function MyAssignInfo() {
   useEffect(() => {
     refetch();
   }, []);
+
+  // 스크롤 기능
+  const [scrollY, setScrollY] = useState(0);
+  const gotoTop = () => {
+    // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 940,
+      behavior: 'smooth',
+    });
+    setScrollY(0); // ScrollY 의 값을 초기화
+  };
+
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow);
+    };
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow);
+    };
+  });
+  console.log('window Scroll From Top:', scrollY);
+
+  useEffect(() => {
+    gotoTop();
+  }, [MyAssignData]);
+
+  //-------------
 
   return (
     <>
--- src/Components/Mypage/Myinfo.jsx
+++ src/Components/Mypage/Myinfo.jsx
@@ -1,5 +1,5 @@
 import { useApiAxios } from 'api/base';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from 'contexts/AuthContext';
 import Sidebar from 'Components/Mypage/Sidebar';
 
@@ -16,6 +16,38 @@ function Myinfo() {
   useEffect(() => {
     refetch();
   }, []);
+
+  // 스크롤 기능
+  const [scrollY, setScrollY] = useState(0);
+  const gotoTop = () => {
+    // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 940,
+      behavior: 'smooth',
+    });
+    setScrollY(0); // ScrollY 의 값을 초기화
+  };
+
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow);
+    };
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow);
+    };
+  });
+  console.log('window Scroll From Top:', scrollY);
+
+  useEffect(() => {
+    gotoTop();
+  }, [userData]);
+
+  //-------------
 
   return (
     <>
