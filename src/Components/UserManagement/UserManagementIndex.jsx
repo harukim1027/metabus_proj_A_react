5d70fa50203174555abb77cc031d9419e87d87a9
Suin Kim <soo.officer@gmail.com>
2022-02-24 18:47:31 +0900

    UserManage css 수정

--- src/Components/UserManagement/UserManagementIndex.jsx
+++ src/Components/UserManagement/UserManagementIndex.jsx
@@ -2,13 +2,15 @@ import { useCallback, useState } from 'react';
 import { useAuth } from 'contexts/AuthContext';
 import { useEffect } from 'react';
 import { useApiAxios } from 'api/base';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
+import '../../App.css';
 import 'css/pagination_userManage.css';
 
 function UserManagementIndex() {
   const [query, setQuery] = useState(null);
   const { auth } = useAuth();
+  const navigate = useNavigate();
 
   // 페이징
   const [, setCurrentItems] = useState(null);
@@ -61,92 +63,126 @@ function UserManagementIndex() {
   };
 
   return (
-    <div>
-      <h2>UserManagementIndex</h2>
-      {loading && '로딩 중 ...'}
-      {error && '로딩 중 에러가 발생했습니다.'}
+    <>
+      <div className="header flex flex-wrap justify-center">
+        <div className="bg-white rounded-xl shadow-md overflow-hidden px-20 pt-5 pb-10 my-10 w-2/3">
+          <blockquote className="mt-5 text-6xl mb-3 font-semibold italic text-center text-slate-900">
+            <span className="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-blue-900 relative inline-block">
+              <span className="relative text-white">" 회원 관리 "</span>
+            </span>
+          </blockquote>
 
-      <input
-        type="text"
-        placeholder="아이디,이름,닉네임으로 검색"
-        onChange={handleChange}
-        onKeyPress={handleKeyPress}
-        className="mt-3 ml-3 border-2 border-gray-300"
-      />
-      <button
-        type="submit"
-        onClick={() => refetch()}
-        className="font-bold py-2 px-4 rounded cursor-pointer ml-1 bg-blue-500 hover:bg-blue-300 text-white"
-      >
-        검색
-      </button>
+          <div className="ml-3 mb-6 mt-3">
+            <div className="text-right">
+              <input
+                type="text"
+                placeholder="아이디,이름,닉네임으로 검색"
+                onChange={handleChange}
+                onKeyPress={handleKeyPress}
+                className="relative rounded p-3 text-m mb-3 bg-gray-100 focus:outline-none focus:border focus:border-gray-400 md:w-1/3 px-3 md:mb-0"
+              />
+              <button
+                type="submit"
+                onClick={() => refetch()}
+                className="relative ml-2 mr-4 flex-shrink-0 bg-blue-700 hover:bg-blue-900 border-blue-700 hover:border-blue-900 text-xl border-4 text-white px-3 py-2 rounded"
+              >
+                검색
+              </button>
+            </div>
+          </div>
 
-      <table className="border-2">
-        <thead className="border-2">
-          <tr>
-            <th className="border-2">유저아이디</th>
-            <th className="border-2">이름</th>
-            <th className="border-2">닉네임</th>
-            <th className="border-2">연락처</th>
-            <th className="border-2">이메일</th>
-            <th className="border-2">거주지역</th>
-          </tr>
-        </thead>
+          {loading && '로딩 중 ...'}
+          {error && '로딩 중 에러가 발생했습니다.'}
 
-        <tbody>
-          {userData &&
-            userData.results.map((user) => (
-              <tr>
-                <td>
-                  <Link to={`/admin/usermanage/${user.userID}/`}>
-                    {user.userID}
-                  </Link>
-                </td>
+          <div className="mb-5">
+            <table className="mb-5 border text-center min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    유저아이디
+                  </th>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    이름
+                  </th>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    닉네임
+                  </th>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    거주지역
+                  </th>
+                </tr>
+              </thead>
 
-                <td>
-                  <Link to={`/admin/usermanage/${user.userID}/`}>
-                    {user.name}
-                  </Link>
-                </td>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {userData &&
+                  userData.results.map((user) => (
+                    <tr>
+                      <td className="py-4">
+                        <div
+                          className="text-lg font-medium text-gray-900 cursor-pointer"
+                          onClick={() =>
+                            navigate(`/admin/usermanage/${user.userID}/`)
+                          }
+                        >
+                          {user.userID}
+                        </div>
+                      </td>
 
-                <td>
-                  <Link to={`/admin/usermanage/${user.userID}/`}>
-                    {user.nickname}
-                  </Link>
-                </td>
+                      <td className="py-4">
+                        <div
+                          className="text-lg font-medium text-gray-900 cursor-pointer"
+                          onClick={() =>
+                            navigate(`/admin/usermanage/${user.userID}/`)
+                          }
+                        >
+                          {user.name}
+                        </div>
+                      </td>
 
-                <td>{user.phone_number}</td>
+                      <td className="py-4">
+                        <div
+                          className="text-lg font-medium text-gray-900 cursor-pointer"
+                          onClick={() =>
+                            navigate(`/admin/usermanage/${user.userID}/`)
+                          }
+                        >
+                          {user.nickname}
+                        </div>
+                      </td>
 
-                <td>{user.email}</td>
+                      <td className="py-4">
+                        <div className="text-lg font-medium text-gray-900">
+                          {user?.region === '서울' && '서울'}
+                          {user?.region === '인천' && '인천'}
+                          {user?.region === '대전' && '대전'}
+                          {user?.region === '세종' && '세종'}
+                          {user?.region === '대구' && '대구'}
+                          {user?.region === '부산' && '부산'}
+                          {user?.region === '광주' && '광주'}
+                          {user?.region === '울산' && '울산'}
+                          {user?.region === '제주' && '제주'}
+                          {user?.region === '강원' && '강원'}
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
 
-                <td>
-                  {user?.region === '서울' && '서울'}
-                  {user?.region === '인천' && '인천'}
-                  {user?.region === '대전' && '대전'}
-                  {user?.region === '세종' && '세종'}
-                  {user?.region === '대구' && '대구'}
-                  {user?.region === '부산' && '부산'}
-                  {user?.region === '광주' && '광주'}
-                  {user?.region === '울산' && '울산'}
-                  {user?.region === '제주' && '제주'}
-                  {user?.region === '강원' && '강원'}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-
-      <ReactPaginate
-        previousLabel="<"
-        breakLabel="..."
-        nextLabel=">"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={itemsPerPage}
-        pageCount={pageCount}
-        renderOnZeroPageCount={null}
-        className="pagination_userManage"
-      />
-    </div>
+          <ReactPaginate
+            previousLabel="<"
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={itemsPerPage}
+            pageCount={pageCount}
+            renderOnZeroPageCount={null}
+            className="pagination_userManage"
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
