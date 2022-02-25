0da66ec88f6cc69f9cd8249143ff88b45b50eb3c
Suin Kim <soo.officer@gmail.com>
2022-02-25 10:03:27 +0900

    회원관리 페이지 css 수정

--- src/Components/UserManagement/UserAssignList.jsx
+++ src/Components/UserManagement/UserAssignList.jsx
@@ -1,17 +1,18 @@
 import { useApiAxios } from 'api/base';
 import { useAuth } from 'contexts/AuthContext';
 import { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import 'css/pagination_userManage.css';
 import '../../App.css';
-import 'css/pagination_userManage.css';
+import './userManage.css';
 
 function UserAssignList() {
   const [query, setQuery] = useState(null);
   const { auth } = useAuth();
   const { userId } = useParams();
+  const navigate = useNavigate();
 
   // 페이징
   const [, setCurrentItems] = useState(null);
@@ -65,54 +66,69 @@ function UserAssignList() {
   return (
     <>
       <div className="header flex flex-wrap justify-center">
-        <div className="userManage_header rounded-xl shadow-md overflow-hidden px-20 pt-5 pb-10 my-10 w-2/3">
-          <blockquote class="mt-5 text-6xl mb-3 font-semibold italic text-center text-slate-900">
-            <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-green-400 relative inline-block">
-              <span class="relative text-white">" 회원관리 "</span>
+        <div className="notice_header rounded-xl shadow-md overflow-hidden px-20 pt-5 pb-10 my-10 w-2/3">
+          <blockquote className="mt-5 text-6xl mb-3 font-semibold italic text-center text-slate-900">
+            <span className="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-green-400 relative inline-block">
+              <span className="relative text-white">" 입양신청 현황 "</span>
             </span>
           </blockquote>
 
           {loading && '로딩 중 ...'}
           {error && '로딩 중 에러가 발생했습니다.'}
 
-          <table className="border-2">
-            <thead className="border-2">
-              <tr>
-                <th className="border-2">번호</th>
-                <th className="border-2">등록번호</th>
-                <th className="border-2">신청날짜</th>
-                <th className="border-2">진행상태</th>
-              </tr>
-            </thead>
-            <tbody>
-              {AssignStatusData?.results.map((assign) => (
+          <div className="mb-5">
+            <table className="mb-5 mt-10 border text-center min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
                 <tr>
-                  <td>
-                    <Link to={`/admin/assignmanage/${assign.assignment_no}/`}>
-                      {assign.assignment_no}
-                    </Link>
-                  </td>
-
-                  <td>
-                    <Link to={`/admin/animal/${assign.animal.animal_no}`}>
-                      {assign.animal.animal_reg_num}
-                    </Link>
-                  </td>
-
-                  <td>{assign.created_at}</td>
-
-                  <td>
-                    {assign.status === '신청' && '신청'}
-                    {assign.status === '심사 중' && '심사 중'}
-                    {assign.status === '수락' && '수락'}
-                    {assign.status === '교육 중' && '교육 중'}
-                    {assign.status === '입양 완료' && '입양 완료'}
-                    {assign.status === '거절' && '거절'}
-                  </td>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    번호
+                  </th>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    등록번호
+                  </th>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    신청날짜
+                  </th>
+                  <th className="px-6 py-3 text-center text-xl font-bold text-gray-500 uppercase tracking-wider w-1/4">
+                    진행상태
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {AssignStatusData?.results.map((assign) => (
+                  <tr>
+                    <td className="py-4">
+                      <div
+                        className="text-xl font-medium text-gray-900"
+                        onClick={() =>
+                          navigate(
+                            `/admin/assignmanage/${assign.assignment_no}/`,
+                          )
+                        }
+                      >
+                        {assign.assignment_no}
+                      </div>
+                    </td>
+
+                    <td className="py-4">
+                      <div
+                        className="text-lg font-medium text-gray-900 cursor-pointer"
+                        onClick={() =>
+                          navigate(`/admin/animal/${assign.animal.animal_no}/`)
+                        }
+                      >
+                        {assign.animal.animal_reg_num}
+                      </div>
+                    </td>
+
+                    <td className="text-lg py-4">{assign.created_at}</td>
+
+                    <td className="text-lg py-4">{assign.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
           <ReactPaginate
             previousLabel="<"
