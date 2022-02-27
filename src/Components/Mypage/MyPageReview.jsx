bf7c95660810c678772f1d176807e9eb802d20b6
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-02-28 08:48:08 +0900

    div 태그 간결하게 수정 : MyPageReview

--- src/Components/Mypage/MyPageReview.jsx
+++ src/Components/Mypage/MyPageReview.jsx
@@ -66,114 +66,110 @@ function MyPageReview() {
 
   return (
     <>
-      <div className="header">
-        <div className="justify-center mx-20">
-          <div className="align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="mypage_header rounded-xl shadow-md">
-              <blockquote className="mt-5 xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl mb-3 font-semibold italic text-center text-slate-900">
-                <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-purple-400 relative inline-block">
-                  <span class="xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl relative text-white">
-                    " 내 입양후기 "
-                  </span>
-                </span>
-              </blockquote>
-              {/* 로딩 에러 */}
-              {loading && (
-                <LoadingIndicator>&nbsp;&nbsp;로딩 중...</LoadingIndicator>
-              )}
-              {error && (
-                <>
-                  <p className="text-red-400">
-                    &nbsp;&nbsp; ! 로딩 중 에러가 발생했습니다. !
-                  </p>
-                </>
-              )}
-              {error?.response?.status === 401 && (
-                <div className="text-red-400">
-                  조회에 실패했습니다. 입력하신 정보를 다시 확인해주세요.
-                </div>
-              )}
-              <div className="mb-5 overflow-hidden border-b border-gray-200">
-                <table className="mt-3 mb-5 mr-5 border text-center min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
-                      >
-                        글 번호
-                      </th>
-                      <th
-                        scope="col"
-                        className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
-                      >
-                        제목
-                      </th>
-                      <th
-                        scope="col"
-                        className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
-                      >
-                        작성자
-                      </th>
-                      <th
-                        scope="col"
-                        className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
-                      >
-                        작성일자
-                      </th>
-                    </tr>
-                  </thead>
-
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {reviewList && (
-                      <>
-                        {reviewList.results
-                          .filter((a) => a.user.userID === auth.userID)
-                          .map((review) => (
-                            <tr key={review.review_no}>
-                              <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs ">
-                                <div className="ml-4">
-                                  <div className="text-sm font-medium text-gray-900">
-                                    {review.review_no}
-                                  </div>
-                                </div>
-                              </td>
-                              <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs">
-                                <span className="px-2 rounded-full bg-green-100 text-green-800 font-semibold">
-                                  <Link to={`/review/${review.review_no}/`}>
-                                    {review.title}
-                                  </Link>
-                                </span>
-                              </td>
-                              <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs">
-                                <Link to={`/review/${review.review_no}/`}>
-                                  {review.user.nickname}
-                                </Link>
-                              </td>
-                              <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs">
-                                {review.created_at}
-                              </td>
-                            </tr>
-                          ))}
-                      </>
-                    )}
-                  </tbody>
-                </table>
-              </div>
+      <div className="header justify-center mx-20 ">
+        <div className="mypage_header rounded-xl shadow-md overflow-hidden sm:px-20 pt-5 pb-10 my-10  xl:w-2/3 lg:w-2/3 md:w-3/4 sm:w-w-full xs:w-full">
+          <blockquote class="mt-5 xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl mb-3 font-semibold italic text-center text-slate-900">
+            <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-purple-400 relative inline-block">
+              <span class="xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl relative text-white">
+                " 내 작성글 "
+              </span>
+            </span>
+          </blockquote>
+          {/* 로딩 에러 */}
+          {loading && (
+            <LoadingIndicator>&nbsp;&nbsp;로딩 중...</LoadingIndicator>
+          )}
+          {error && (
+            <>
+              <p className="text-red-400">
+                &nbsp;&nbsp; ! 로딩 중 에러가 발생했습니다. !
+              </p>
+            </>
+          )}
+          {error?.response?.status === 401 && (
+            <div className="text-red-400">
+              조회에 실패했습니다. 입력하신 정보를 다시 확인해주세요.
             </div>
-            <ReactPaginate
-              previousLabel="<"
-              breakLabel="..."
-              nextLabel=">"
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={itemsPerPage}
-              pageCount={pageCount}
-              renderOnZeroPageCount={null}
-              className="pagination_notice"
-            />
-            <br />
+          )}
+          <div className="mb-5 overflow-hidden border-b border-gray-200">
+            <table className="mt-3 mb-5 mr-5 border text-center min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
+                  >
+                    글 번호
+                  </th>
+                  <th
+                    scope="col"
+                    className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
+                  >
+                    제목
+                  </th>
+                  <th
+                    scope="col"
+                    className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
+                  >
+                    작성자
+                  </th>
+                  <th
+                    scope="col"
+                    className="xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72"
+                  >
+                    작성일자
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody className="bg-white divide-y divide-gray-200">
+                {reviewList && (
+                  <>
+                    {reviewList.results
+                      .filter((a) => a.user.userID === auth.userID)
+                      .map((review) => (
+                        <tr key={review.review_no}>
+                          <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs ">
+                            <div className="ml-4">
+                              <div className="text-sm font-medium text-gray-900">
+                                {review.review_no}
+                              </div>
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs">
+                            <span className="px-2 rounded-full bg-green-100 text-green-800 font-semibold">
+                              <Link to={`/review/${review.review_no}/`}>
+                                {review.title}
+                              </Link>
+                            </span>
+                          </td>
+                          <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs">
+                            <Link to={`/review/${review.review_no}/`}>
+                              {review.user.nickname}
+                            </Link>
+                          </td>
+                          <td className="px-6 py-4 xl:text-xl lg:text-xl md:text-m sm:text-s xs:text-xs">
+                            {review.created_at}
+                          </td>
+                        </tr>
+                      ))}
+                  </>
+                )}
+              </tbody>
+            </table>
           </div>
         </div>
+        <ReactPaginate
+          previousLabel="<"
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={itemsPerPage}
+          pageCount={pageCount}
+          renderOnZeroPageCount={null}
+          className="pagination_notice"
+        />
+        <br />
       </div>
     </>
   );
