62173491c72e4a9cd31da1c6735238e93a299792
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-02-28 08:45:26 +0900

    div 태그 간결하게 수정

--- src/Components/Mypage/Myinfo.jsx
+++ src/Components/Mypage/Myinfo.jsx
@@ -36,94 +36,90 @@ function Myinfo() {
 
   return (
     <>
-      <div className="header">
-        <div className="justify-center mx-20">
-          <div className="align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="mypage_header rounded-xl shadow-md">
-              <blockquote class="mt-5 xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl mb-3 font-semibold italic text-center text-slate-900">
-                <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-blue-900 relative inline-block">
-                  <span class="xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl relative text-white">
-                    " 내 회원정보 "
-                  </span>
-                </span>
-              </blockquote>
-              {/* 로딩 에러 */}
-              {loading && (
-                <LoadingIndicator>&nbsp;&nbsp;로딩 중...</LoadingIndicator>
-              )}
-              {error && (
-                <>
-                  <p className="text-red-400">
-                    &nbsp;&nbsp; ! 로딩 중 에러가 발생했습니다. !
-                  </p>
-                </>
-              )}
-              {error?.response?.status === 401 && (
-                <div className="text-red-400">
-                  조회에 실패했습니다. 입력하신 정보를 다시 확인해주세요.
-                </div>
-              )}
-
-              <div className="mb-5 overflow-hidden border-b border-gray-200">
-                <table className="mb-5 mr-5 mt-3 border text-center min-w-full divide-y divide-gray-200">
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72">
-                      이름
-                    </th>
-                    <td>{userData?.name}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72">
-                      아이디
-                    </th>
-                    <td>{userData?.userID}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72">
-                      닉네임
-                    </th>
-                    <td>{userData?.nickname}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
-                      이메일
-                    </th>
-                    <td>{userData?.email}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
-                      연락처
-                    </th>
-                    <td>{userData?.phone_number}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
-                      거주지
-                    </th>
-                    <td>{userData?.region}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
-                      비밀번호 퀴즈
-                    </th>
-                    <td>{userData?.password_quiz}</td>
-                  </tr>
-
-                  <tr>
-                    <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
-                      퀴즈 정답
-                    </th>
-                    <td>{userData?.password_quiz_answer}</td>
-                  </tr>
-                </table>
-              </div>
+      <div className="header justify-center mx-20 ">
+        <div className="mypage_header rounded-xl shadow-md overflow-hidden sm:px-20 pt-5 pb-10 my-10  xl:w-2/3 lg:w-2/3 md:w-3/4 sm:w-w-full xs:w-full">
+          <blockquote class="mt-5 xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl mb-3 font-semibold italic text-center text-slate-900">
+            <span class="mt-7 mb-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-blue-900 relative inline-block">
+              <span class="xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl xs:text-xl relative text-white">
+                " 내 회원정보 "
+              </span>
+            </span>
+          </blockquote>
+          {/* 로딩 에러 */}
+          {loading && (
+            <LoadingIndicator>&nbsp;&nbsp;로딩 중...</LoadingIndicator>
+          )}
+          {error && (
+            <>
+              <p className="text-red-400">
+                &nbsp;&nbsp; ! 로딩 중 에러가 발생했습니다. !
+              </p>
+            </>
+          )}
+          {error?.response?.status === 401 && (
+            <div className="text-red-400">
+              조회에 실패했습니다. 입력하신 정보를 다시 확인해주세요.
             </div>
+          )}
+
+          <div className="mb-5 overflow-hidden border-b border-gray-200">
+            <table className="mb-5 mr-5 mt-3 border text-center min-w-full divide-y divide-gray-200">
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72">
+                  이름
+                </th>
+                <td>{userData?.name}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72">
+                  아이디
+                </th>
+                <td>{userData?.userID}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center  font-bold text-gray-500 uppercase tracking-wider w-72">
+                  닉네임
+                </th>
+                <td>{userData?.nickname}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
+                  이메일
+                </th>
+                <td>{userData?.email}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
+                  연락처
+                </th>
+                <td>{userData?.phone_number}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
+                  거주지
+                </th>
+                <td>{userData?.region}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
+                  비밀번호 퀴즈
+                </th>
+                <td>{userData?.password_quiz}</td>
+              </tr>
+
+              <tr>
+                <th className="xl:text-xl lg:text-xl md:text-m sm:text-m xs:text-s border border-slate-200 bg-gray-50 px-4 py-3 text-center font-bold text-gray-500 uppercase tracking-wider w-72">
+                  퀴즈 정답
+                </th>
+                <td>{userData?.password_quiz_answer}</td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
